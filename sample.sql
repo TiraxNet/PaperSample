61c13daedcd34fceda875058c0580aa89607a442
Saadatfar <mh.saadatfar@gmail.com>
2013-11-13 23:00:45 +0330

    Widgets are part of SQL structure.

--- sample.sql
+++ sample.sql
@@ -12,15 +12,3 @@ INSERT INTO `blocks` (`id`, `tmp`, `parent`, `name`, `x1`, `y1`, `x2`, `y2`, `wi
 INSERT INTO `templates` (`id`, `name`, `version`, `title`, `parent`, `css`) VALUES
 (1, 'Home_Page', '1', 'Paper!', 0, '#PapaDIV{text-align:center;background:#ffffff;}Body{background:#ffffff;}');
 
---
--- Dumping data for table `widgets`
---
-
-INSERT INTO `widgets` (`id`, `name`, `pathname`) VALUES
-(1, 'None', 'none'),
-(2, 'Link', 'link'),
-(3, 'Form', 'form'),
-(4, 'HTML', 'html'),
-(5, 'InputBox', 'input'),
-(6, 'Submit Button', 'submit');
-
